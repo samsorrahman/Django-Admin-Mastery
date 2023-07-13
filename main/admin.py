96295908a8ecd9ee9ca3bee8d8c70592bbeb2206
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-07-12 22:16:01 -0700

    custom ordering

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,16 @@
+from typing import Any, List, Tuple, Union
 from django.contrib import admin
+from django.http.request import HttpRequest
 from .models import Blog
 # Register your models here.
 
 class BlogAdmin(admin.ModelAdmin):
     list_display = ['title', 'date_created', 'last_modified', 'is_draft']
     list_filter = ['is_draft']
+    
+    def get_ordering(self, request):
+        if request.user.is_superuser:
+            return ('title', '-date_created')
+        
+        return ('title')
 admin.site.register(Blog, BlogAdmin)
