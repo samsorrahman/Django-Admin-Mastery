027d60ee4d75a5592f5837aac5691c9494527cde
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-07-12 22:54:23 -0700

    custom admin change list actions

--- main/admin.py
+++ main/admin.py
@@ -2,6 +2,7 @@ from typing import Any, List, Tuple, Union
 from django.contrib import admin
 from django.http.request import HttpRequest
 from .models import Blog
+from django.contrib import messages
 # Register your models here.
 
 class BlogAdmin(admin.ModelAdmin):
@@ -10,6 +11,7 @@ class BlogAdmin(admin.ModelAdmin):
     search_fields = ['title']
     prepopulated_fields = {'slug': ('title',)}
     list_per_page = 20
+    actions = ('set_blogs_to_published',)
     
     def get_ordering(self, request):
         if request.user.is_superuser:
@@ -17,5 +19,9 @@ class BlogAdmin(admin.ModelAdmin):
         
         return ('title')
     
+    def set_blogs_to_published(self, request, queryset):
+        count = queryset.update(is_draft=False)
+        self.message_user(request, '{} blogs have been published successfully.'.format(count), messages.SUCCESS)
+    set_blogs_to_published.short_description= 'Mark selected blog as published'
     
 admin.site.register(Blog, BlogAdmin)
