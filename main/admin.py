dfec95300a38dc62ea7e1de3495e6e15b2ab619f
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-07-12 22:10:37 -0700

    list filter and random data

--- main/admin.py
+++ main/admin.py
@@ -4,5 +4,5 @@ from .models import Blog
 
 class BlogAdmin(admin.ModelAdmin):
     list_display = ['title', 'date_created', 'last_modified', 'is_draft']
-    
+    list_filter = ['is_draft']
 admin.site.register(Blog, BlogAdmin)
