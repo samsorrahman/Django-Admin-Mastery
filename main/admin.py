6421f996188765ee39d59d7cb9b6939f115f1429
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-07-12 22:18:39 -0700

    custom search

--- main/admin.py
+++ main/admin.py
@@ -7,10 +7,12 @@ from .models import Blog
 class BlogAdmin(admin.ModelAdmin):
     list_display = ['title', 'date_created', 'last_modified', 'is_draft']
     list_filter = ['is_draft']
-    
+    search_fields = ['title']
     def get_ordering(self, request):
         if request.user.is_superuser:
             return ('title', '-date_created')
         
         return ('title')
+    
+    
 admin.site.register(Blog, BlogAdmin)
